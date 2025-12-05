c5c64ab812434e5e93dfea4435416c5c07f4261f
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-12-05 09:00:20 -0600

    initial commit - weather app with tailwind setup

---
+++ expenses/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-red-500 text-white p-6 text-3xl font-bold">
+      Tailwind is Working! 🎉
+    </div>
+  );
+}
+
+export default App;
+
